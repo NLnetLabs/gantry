951870bed4bc98b0010653a762be199b61386834
ximon18 <3304436+ximon18@users.noreply.github.com>
2019-05-09 11:57:49 +0200

    FIX: "/home/travis/.travis/functions: line 104:  ./gantry: No such file or directory" caused by incorrect Travis CI YAML syntax. Move the commands into a script file and avoid working with YAML.

---
+++ travis/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# See: https://docs.travis-ci.com/user/job-lifecycle#complex-build-commands
+set -ev
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  ./gantry --version
+  ./gantry deploy --region nyc1 routinator vr-sros:16.0.R6 vr-vmx:18.2R1.9
+  ./gantry status
+  ./gantry --data-dir tests test all
+fi
