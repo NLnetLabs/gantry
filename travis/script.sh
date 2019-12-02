805ac80607b7e0a2bc4885bd2910c42ed3b22b6e
ximon18 <3304436+ximon18@users.noreply.github.com>
2019-12-02 12:39:22 +0100

    Dump container logs on test failure.

--- travis/script.sh
+++ travis/script.sh
@@ -2,8 +2,25 @@
 # See: https://docs.travis-ci.com/user/job-lifecycle#complex-build-commands
 set -evx
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  COMPONENTS_TO_TEST="routinator vr-sros:16.0.R6 vr-vmx:18.2R1.9"
+
   ./gantry --version
-  ./gantry deploy --region nyc1 routinator vr-sros:16.0.R6 vr-vmx:18.2R1.9 || true
+
+  # Don't abort on deployment failure, run whatever tests we can e.g. if one
+  # router fails to deploy but others succeed.
+  ./gantry deploy --region nyc1 ${COMPONENTS_TO_TEST} || true
+
+  # Dump the deployment status so the subsequent test results can be understood
+  # in the context of which components succeeded or failed to deploy.
   ./gantry status
-  ./gantry --data-dir tests test all
+
+  # Run the tests. On failure dump logs to enable root cause analysis.
+  if ! ./gantry --data-dir tests test all; then
+    for C in ${COMPONENTS_TO_TEST}; do
+        echo "LOGS FOR GANTRY COMPONENT: ${C}: START"
+        ./gantry logs $C || true
+        echo "LOGS FOR GANTRY COMPONENT: ${C}: END"
+    done
+    exit 1
+  fi
 fi
